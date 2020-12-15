2c199038ba67bb673b7592955f75f759ccdf465e
Shaw S. Yu <46137960+AhoyKakkoii@users.noreply.github.com>
2020-12-15 12:24:59 -0500

    update for daphne

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -1,11 +1,15 @@
 import os
+import django
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+django.setup()
+
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
