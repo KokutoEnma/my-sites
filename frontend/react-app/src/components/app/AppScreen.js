cc5fefd39614cc72baa5e3192a27fa3737821471
Shaw S. Yu <46137960+AhoyKakkoii@users.noreply.github.com>
2020-12-17 01:48:09 -0500

    update app screent

--- frontend/react-app/src/components/app/AppScreen.js
+++ frontend/react-app/src/components/app/AppScreen.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import Button from '@material-ui/core/Button';
+import { motion } from 'framer-motion'
 
 class AppScreen extends React.Component {
 
     render() {
+        const wrapper_height = window.innerHeight - 84 * 2;
         return (
-            <>
-                AppScreen
-                <Button variant="contained" onClick={()=>this.props.history.push('/app/chat')}>Chat</Button>
-
-            </>
+            <motion.div className="content-wrapper" initial={{ opacity: 0, minHeight: wrapper_height }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
+                <div className="app-button">
+                    <Button variant="contained" onClick={() => this.props.history.push('/app/chat')}>Chat</Button>
+                </div>
+                <div className="app-button">
+                    <Button variant="contained" onClick={() => window.location.replace('http://delit.shaw-yu.com')}>Delit</Button>
+                </div>
+            </motion.div>
         )
     }
 }
--- frontend/react-app/src/index.js
+++ frontend/react-app/src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import './index.css'
 import './components/tools/tools.css'
 import './components/chat/chat.css'
+import './components/app/app.css'
+
 
 ReactDOM.render(
     <React.StrictMode>
