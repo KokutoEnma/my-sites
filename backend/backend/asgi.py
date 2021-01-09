1bddacc8149e680fccae1fa14902adfc6df11a1d
Shaw S. Yu <46137960+AhoyKakkoii@users.noreply.github.com>
2021-01-09 00:31:59 -0500

    Update asgi.py

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.backend.settings")
 django.setup()
 
 from django.core.asgi import get_asgi_application
