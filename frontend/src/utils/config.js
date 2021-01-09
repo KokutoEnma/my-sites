0de8c4a8e564cb21341ffd60793fff1beeaf6937
Shaw S. Yu <46137960+AhoyKakkoii@users.noreply.github.com>
2021-01-09 00:45:13 -0500

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,6 +1,6 @@
 export const urls = {
-    frontend: process.env.ENVIRONMENT === 'production' ? 'www.shaw-yu.com' : 'localhost:3000',
-    backend: process.env.ENVIRONMENT === 'production' ? 'www.shaw-yu.com' : 'localhost:8000',
+    frontend: process.env.NODE_ENV === 'production' ? 'www.shaw-yu.com' : 'localhost:3000',
+    backend: process.env.NODE_ENV === 'production' ? 'www.shaw-yu.com' : 'localhost:8000',
 }
 
 
